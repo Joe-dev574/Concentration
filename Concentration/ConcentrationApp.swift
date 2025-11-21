b14aa88a381b2e8286a52e289272524313e276e8
Rangerboy574 <jdeweese574@icloud.com>
2025-11-21 12:14:35 -0600

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Joseph DeWeese on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcentrationTests/ConcentrationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationTests.swift
+//  ConcentrationTests
+//
+//  Created by Joseph DeWeese on 11/21/25.
+//
+
+import Testing
+@testable import Concentration
+
+struct ConcentrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
